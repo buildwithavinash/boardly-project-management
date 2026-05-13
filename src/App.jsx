ad6780e6ad76fee69fc1eafd976a34fa9148486a
Avinash <buildwithavinash@users.noreply.github.com>
2026-05-13 22:26:38 +0530

    Feat: Signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router"
 import Signup from "./pages/Signup"
 
 const App = () => {
   return (
-    <div>
-      <Signup/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Signup/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/login" element={<div>Login Page</div>}/>
+    </Routes>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,65 +1,127 @@
-/* 
-    User opens this page
-    Sees a form with name, email, password, role(dropdown) field..
-    the states are going to be name, email, password, role but instead of making so many state we can use a master object to collect all in one.. 
-    then on submit, the whole data is sent as an object to supbase.. 
-*/
-
-import { useState } from "react"
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useNavigate } from "react-router";
 
 const Signup = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        role: 'admin'
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "admin",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setError(null)
+
+    // supabase auth signup
+
+    const {data, error} = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password
     })
 
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    if(error) {
+    setError(error.message)
+    setLoading(false)
+    return
+  }
 
-    const handleChange = (e) => {
-        const {name, value} = e.target
-        setFormData(formData => ({...formData, [name]: value}))
-    }
+  // Insert to the profiles table
+  const {error: profileError} = await supabase.from('profiles').insert({
+    id: data.user.id,
+    name: formData.name,
+    role: formData.role
+  })
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  if(profileError){
+    setError(profileError.message);
+    setLoading(false)
+    return
+  }
 
-        console.log(formData);
+//   reset form
+setFormData({
+    name:'', email: '', password: '', role: ''
+})
 
-        setFormData({
-            name: '',
-        email: '',
-        password: '',
-        role: 'admin'
-        })
-    }
+navigate('/login')
+// redirect to login
+alert("Signup successful! Please Login.")
+  }
+
+  
+
   return (
     <div className="p-4">
-        <div>
-            <h2 className="text-3xl font-semibold">Signup</h2>
+      <div>
+        {error && <p>{error}</p>}
+        <h2 className="text-3xl font-semibold">Signup</h2>
 
-            <form onSubmit={handleSubmit} className="border border-slate-300 flex flex-col items-center justify-center gap-4 p-8 rounded-md mt-8">
+        <form
+          onSubmit={handleSubmit}
+          className="border border-slate-300 flex flex-col items-center justify-center gap-4 p-8 rounded-md mt-8"
+        >
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Name"
+            className="border border-slate-300 px-4 py-2 rounded-md"
+          />
 
-                <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" className="border border-slate-300 px-4 py-2 rounded-md" />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="you@example.com"
+            onChange={handleChange}
+            className="border border-slate-300 px-4 py-2 rounded-md"
+          />
 
-                <input type="email" name="email" value={formData.email} placeholder="you@example.com" onChange={handleChange} className="border border-slate-300 px-4 py-2 rounded-md" />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            placeholder="Password"
+            onChange={handleChange}
+            className="border border-slate-300 px-4 py-2 rounded-md"
+          />
 
-                <input type="password" name="password" value={formData.password} placeholder="Password" onChange={handleChange} className="border border-slate-300 px-4 py-2 rounded-md" />
-
-                <div>
-                    <label htmlFor="">Signup as : </label>
-                    <select name="role" value={formData.role} onChange={handleChange} className="border border-slate-300 rounded-md px-2 py-1">
-                        <option value="admin">Admin</option>
-                        <option value="member">Member</option>
-                    </select>
-                </div>
-                <button type="submit" className="bg-black text-white px-4 py-2 rounded-md">Signup</button>
-            </form>
-        </div>
+          <div>
+            <label htmlFor="">Signup as : </label>
+            <select
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
+              className="border border-slate-300 rounded-md px-2 py-1"
+            >
+              <option value="admin">Admin</option>
+              <option value="member">Member</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-black text-white px-4 py-2 rounded-md"
+          >
+            Signup
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
