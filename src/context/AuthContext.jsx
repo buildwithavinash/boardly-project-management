e3284665135abbab3f39825e1f98fc4d1c1c4903
Avinash <buildwithavinash@users.noreply.github.com>
2026-05-18 12:25:41 +0530

    Feat: getting session

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,32 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+import { data } from "react-router-dom";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({children}) => {
     const [ user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        supabase.auth.getSession().then(({data : {session} }) => {
+            setUser(session?.user ?? null);
+            setLoading(false);
+        })
+
+        // session change
+
+        const {data: {subscription}} = supabase.auth.onAuthStateChange((event, session) => {
+            setUser(session?.user ?? null);
+            setLoading(false)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
     
     console.log('User: ', user);
     return (
-        <AuthContext.Provider value={{user, setUser}}>
+        <AuthContext.Provider value={{user, setUser, loading}}>
             {children}
         </AuthContext.Provider>
     )
