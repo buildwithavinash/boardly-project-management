04139e1704baa2c4470e20ab0ae8529d333a6864
Avinash <buildwithavinash@users.noreply.github.com>
2026-05-20 12:39:07 +0530

    Feat: Project services

---
+++ src/services/ProjectService.js
@@ -0,0 +1,21 @@
+import { supabase } from "../lib/supabase"
+
+export const createProject = async (projectData) => {
+    const { data, error } = await supabase.from('projects').insert({...projectData})
+    return { data, error }
+}
+
+export const getProjects = async () => {
+    const { data, error } = await supabase.from('projects').select('*')
+    return { data, error }
+}
+
+export const updateProject = async (id, updatedData) => {
+    const { data, error } = await supabase.from('projects').update({...updatedData}).eq('id', id)
+    return { data, error }
+}
+
+export const deleteProject = async (id) => {
+    const { data, error } = await supabase.from('projects').delete().eq('id', id)
+    return { data, error }
+}
